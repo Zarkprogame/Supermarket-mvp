b4c14c417444efd71b16593b83ca351206aa4ed1
zarkprogame <zarkprogame@gmail.com>
2023-10-21 05:45:25 -0500

    IProviderView

---
+++ Supermarket-mvp/Supermarket-mvp/Views/IProviderView.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp.Views
+{
+    internal interface IProviderView
+    {
+        string ProviderNit { get; set; }
+        string ProviderName { get; set; }
+        string ProviderAddress { get; set; }
+        string ProviderNumber { get; set; }
+
+        string SearchValue { get; set; }
+        bool IsEdit { get; set; }
+        bool IsSuccessful { get; set; }
+        string Message { get; set; }
+
+        event EventHandler SearchEvent;
+        event EventHandler AddNewEvent;
+        event EventHandler EditEvent;
+        event EventHandler DeleteEvent;
+        event EventHandler SaveEvent;
+        event EventHandler CancelEvent;
+
+        void SetProviderListBinldingSource(BindingSource payModeList);
+        void Show();
+    }
+}
