65a9823e96952b9565e9ee50362b854291cb4510
zarkprogame <zarkprogame@gmail.com>
2023-10-20 20:31:22 -0500

    Category Model

--- Supermarket-mvp/Supermarket-mvp/Models/CategoryModel.cs
+++ Supermarket-mvp/Supermarket-mvp/Models/CategoryModel.cs
@@ -1,21 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel;
 
 namespace Supermarket_mvp.Models
 {
-    internal class PayModeModel
+    internal class CategoryModel
     {
         [DisplayName("Category Id")]
         public int Id { get; set; }
 
         [DisplayName("Category Name")]
         [Required(ErrorMessage = "Category Name is Required")]
-        [StringLength(50,MinimumLength = 3, ErrorMessage = "Category Name must be between 3 and 50 Characters")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Category Name must be between 3 and 50 Characters")]
         public string Name { get; set; }
 
         [DisplayName("Description")]
